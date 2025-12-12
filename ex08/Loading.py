640049fdc14a3bce89ddbcfa4d9ae25929fee7d4
Yuna Shimomura <yushimom@f4r14s10.clusters.42paris.fr>
2025-12-12 17:06:02 +0100

    [fix] rm time and sys

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -1,6 +1,4 @@
-import sys
 import os
-import time
 from time import sleep
 from tqdm import tqdm
 
@@ -36,65 +34,47 @@ def ft_tqdm(lst: range):
         The elements of the input iterable 'lst'.
     """
     total = len(lst)
-    start_time = time.time()
 
     try:
         terminal_width = os.get_terminal_size().columns
     except OSError:
         terminal_width = 80
 
-    NON_BAR_LENGTH = 50
+    NON_BAR_LENGTH = 45
     BAR_WIDTH = terminal_width - NON_BAR_LENGTH
     BAR_WIDTH = max(10, BAR_WIDTH)
 
     for i, item in enumerate(lst):
         current_iter = i + 1
-        elapsed_time = time.time() - start_time
         percent = (current_iter / total) * 100
         filled_len = int(BAR_WIDTH * current_iter // total)
 
         # create progress bar
-        bar = '█' * filled_len
+        bar = '=' * filled_len
         if filled_len < BAR_WIDTH:
             bar += '>'
             bar += ' ' * (BAR_WIDTH - len(bar))
 
-        # speed and eta
-        if current_iter > 0 and elapsed_time > 0:
-            speed = current_iter / elapsed_time
-            eta = (total - current_iter) / speed
-        else:
-            speed = 0
-            eta = 0
-
         # create output string
         output = (
             f"{percent:3.0f}%|"
-            f"{bar}| "
+            f"[{bar}]| "
             f"{current_iter}/{total} "
-            f"[{format_time(elapsed_time)}<{format_time(eta)},"
-            f" {speed:5.2f}it/s]"
         )
 
         # back to the head by \r  and overwrite
-        sys.stdout.write('\r' + output)
-        sys.stdout.flush()
+        print('\r' + output, end='', flush=True)
 
         yield item
 
     # lastline
-    elapsed_time = time.time() - start_time
-    final_speed = total / elapsed_time if elapsed_time > 0 else 0
-    final_bar = '█' * BAR_WIDTH
+    final_bar = '=' * BAR_WIDTH
     final_output = (
         f"100%|"
-        f"{final_bar}| "
+        f"[{final_bar}>]| "
         f"{total}/{total} "
-        f"[{format_time(elapsed_time)}<00:00, {final_speed:5.2f}it/s]"
     )
-    sys.stdout.write('\r' + final_output)
-    sys.stdout.write('\n')
-    sys.stdout.flush()
+    print('\r' + final_output, flush=True)
 
 
 def main():
